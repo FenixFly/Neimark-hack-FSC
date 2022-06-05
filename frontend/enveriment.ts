2488dee697dd86e8b50f21ed303ab5296b2d46c1
Vasiliev Evgenii <eugene.unn@gmail.com>
2022-06-05 12:41:31 +0300

    fix

--- frontend/enveriment.ts
+++ frontend/enveriment.ts
@@ -1,2 +1,2 @@
-export const API = 'http://localhost:5000/';
-// export const API = 'https://casein-2022-neymark.herokuapp.com/';
+//export const API = 'http://194.58.123.92:8711/';
+ export const API = 'http://localhost:8711/';
